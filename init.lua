663eb2a3417d7ba9c35f330d11c4ab711daf916c
Simonas <Simonas.Tumsys@ibm.com>
2024-08-26 20:58:05 +0300

    added example config

--- init.lua
+++ init.lua
@@ -2,22 +2,22 @@ hs.ipc.cliInstall()
 hs.loadSpoon("WorkspaceLoader")
 hs.loadSpoon("BundleIdFinder")
 
-
+-- Open applications and attempt moving them to their pre-configured spaces
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "A", function ()
     spoon.WorkspaceLoader:createSpacesAndArrange(false)
 end)
 
-
+-- Rearrange open applications to pre-configured spaces
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "O", function ()
     spoon.WorkspaceLoader:createSpacesAndArrange(true)
 end)
 
-
+-- Find application BundleIDs
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", function ()
     spoon.BundleIdFinder:search()
 end)
 
-
+-- Load a different configuration file
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function ()
     spoon.WorkspaceLoader:chooseConfig()
 end)
