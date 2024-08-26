4891d4596d95cab5646ade5e3f99d174a7a5978c
Simonas <Simonas.Tumsys@ibm.com>
2024-08-26 18:33:24 +0300

    cleaned upp WorkspaceLoader

--- Spoons/WorkspaceLoader.spoon/init.lua
+++ Spoons/WorkspaceLoader.spoon/init.lua
@@ -65,59 +65,6 @@ local function openOrFocusApp(appBundleId)
 end
 
 
-function obj:findBundleIdByKeyword(keyword)
-    local command = string.format([[
-        mdfind "kMDItemContentType == 'com.apple.application-bundle'" |
-        grep -i %s |
-        while read -r app; do
-        defaults read "$app/Contents/Info.plist" CFBundleIdentifier
-    done
-    ]], keyword)
-
-    return hs.execute(command)
-end
-
-
-function showSelectionDialog(items)
-    local choices = {}
-    for i, item in ipairs(items) do
-        table.insert(choices, {
-            text = item,
-            index = i
-        })
-    end
-
-    local chooser = hs.chooser.new(function(choice)
-        if not choice then return end
-        local selectedItem = items[choice.index]
-        hs.pasteboard.setContents(selectedItem)
-        hs.alert.show("Copied to clipboard: " .. selectedItem)
-    end)
-
-    chooser:choices(choices)
-    chooser:width(30)
-    chooser:rows(#choices)
-
-    chooser:show()
-end
-
-
-local function splitByNewline(str)
-    local t = {}
-    for line in str:gmatch("[^\r\n]+") do
-        table.insert(t, line)
-    end
-    return t
-end
-
-
-function obj:searchForAppBundleId() 
-    local _, hint = hs.dialog.textPrompt("Provide search hint for App Name", "hint")
-    local result = splitByNewline(self:findBundleIdByKeyword(hint))
-    showSelectionDialog(result)
-end
-
-
 local function getWaitInterval(shouldOpenApps)
     if shouldOpenApps then return WAIT_INTERVAL else return 0 end
 end
