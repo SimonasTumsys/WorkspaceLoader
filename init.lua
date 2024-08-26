585e2aa767a30bc76a633f190e50b722bd035876
Simonas <Simonas.Tumsys@ibm.com>
2024-08-26 21:20:06 +0300

    arranged comments in init.lua

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@ hs.ipc.cliInstall()
 hs.loadSpoon("WorkspaceLoader")
 hs.loadSpoon("BundleIdFinder")
 
--- Open applications and attempt moving them to their pre-configured spaces
+-- Rearrange open applications to pre-configured spaces
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "A", function ()
     spoon.WorkspaceLoader:createSpacesAndArrange(false)
 end)
 
--- Rearrange open applications to pre-configured spaces
+-- Open applications and attempt moving them to their pre-configured spaces
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "O", function ()
     spoon.WorkspaceLoader:createSpacesAndArrange(true)
 end)
